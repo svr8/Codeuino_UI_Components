502ddd7cf12b18cf11fff4fb4a41c5b080b7d898
svr8 <shikhar.vaish90@gmail.com>
2019-03-29 18:51:10 +0530

    Updated UI

---
+++ script.js
@@ -0,0 +1,16 @@
+var vue = new Vue({
+  el: ".comments-list",
+  data: {
+    comment_list: [
+      { username: 'user1', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'svr8', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'codeuino', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'codeuino', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'codeuino', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'codeuino', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'codeuino', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'codeuino', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+      { username: 'codeuino', body: 'Lorem Ipsum is simply dummy text of the printing and t'},
+    ]
+  }
+})
